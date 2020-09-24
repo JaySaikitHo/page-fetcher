510bcca39af687dca2dd1148f56b275c478075a9
vagrant <ttjho@hotmail.com>
2020-09-24 04:25:33 +0000

    used fs and request to download html from a website

--- fetcher.js
+++ fetcher.js
@@ -1,1 +1,17 @@
+const userInput = process.argv.splice(2)
+const request = require('request');
+const fs = require('fs')
+const size = fs.statSync(`${userInput[1]}`)
 
+
+request(`${userInput[0]}`, (error, response, body) => {
+  fs.writeFile(`${userInput[1]}`, body, (err) => {
+    if (err) throw err;
+    console.log(`downloaded and saved ${size.size} bits to ${userInput[1]} `);
+  });
+});
+
+// fs.writeFile('message.txt', data, (err) => {
+//   if (err) throw err;
+//   console.log('The file has been saved!');
+// });
